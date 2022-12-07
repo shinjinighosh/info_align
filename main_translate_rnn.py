b65c3a1ef0127c9b3342f8f8c8269b627ea87e35
Shinjini Ghosh <shinghos@mit.edu>
2022-12-07 02:12:48 -0500

    debug

--- main_translate_rnn.py
+++ main_translate_rnn.py
@@ -46,12 +46,12 @@ def main():
     for en, es in data:
         padded_en = en + [-1] * (18 - len(en))
         padded_es = es + [-1] * (18 - len(en))
-        data_padded.append((padded_en, padded_es,))
+        data_padded.append([padded_en, padded_es])
 
     for en, es in test_data:
         padded_en = en + [-1] * (18 - len(en))
         padded_es = es + [-1] * (18 - len(en))
-        test_data_padded.append((padded_en, padded_es,))
+        test_data_padded.append([padded_en, padded_es])
 
     # for (word1, word2) in data:
     #     print(vocab.decode(word1))
