5c57e9bfb95a89846dd2d12248b6fe07c3e0995d
Shinjini Ghosh <shinghos@mit.edu>
2022-12-08 03:21:03 -0500

    debug

--- model.py
+++ model.py
@@ -166,7 +166,7 @@ class RNNModel(nn.Module):
         super(RNNModel, self).__init__()
         self.hidden_dim = hidden_dim
         self.n_layers = n_layers
-        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=False)
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)
         self.fc = nn.Linear(hidden_dim, output_size)
 
     def forward(self, x):
