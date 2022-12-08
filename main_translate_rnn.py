5b5805be673f6b20cce171fd54f1223ee8f8b98f
Shinjini Ghosh <shinghos@mit.edu>
2022-12-08 03:03:45 -0500

    debug

--- main_translate_rnn.py
+++ main_translate_rnn.py
@@ -82,6 +82,7 @@ def main():
                 data_1.append(example_inp.type(torch.FloatTensor))
                 target_1.append(example_op.type(torch.FloatTensor))
             data_1 = torch.stack(data_1).to(device)
+            print(data_1.shape)
             target_1 = torch.stack(target_1).to(device)
             optimizer.zero_grad()  # Clears existing gradients from previous epoch
             output, hidden = model(data_1)
